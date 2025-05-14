cb2b34b99a60b6bc02f94883702b9dbda24fc770
yasirnabil534 <yasir.nabil5343@gmail.com>
2025-05-14 12:32:50 +0600

    Yasir-004: Sub portfolio done

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './module/user/user.module';
+import { SubPortfolioModule } from './module/sub-portfolio/sub-portfolio.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { UserModule } from './module/user/user.module';
       isGlobal: true,
     }),
     UserModule,
+    SubPortfolioModule,
   ],
   controllers: [AppController],
   providers: [AppService],
