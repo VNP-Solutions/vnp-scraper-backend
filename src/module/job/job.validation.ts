943b2c8db15437b83ffe7a6147eed8fc62a5fadb
yasirnabil534 <yasir.nabil5343@gmail.com>
2025-11-19 17:26:49 +0600

    Commit-Yasir: DB Parser duration field added

--- src/module/job/job.validation.ts
+++ src/module/job/job.validation.ts
@@ -49,6 +49,7 @@ export const createJobSchema = z.object({
   log_link: z.string().optional().nullable(),
   live_url: z.string().optional().nullable(),
   batch_id: z.union([objectIdSchema, z.null(), z.undefined()]).optional(),
+  db_billing_duration: z.number().int().optional().nullable(),
 });
 
 export const updateJobSchema = createJobSchema.partial();
