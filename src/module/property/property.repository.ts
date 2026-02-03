67bdf10278ebfb907acd13d78d6ee636bb255ffd
yasirnabil534 <yasir.nabil5343@gmail.com>
2026-02-03 14:34:49 +0600

    Commit-Yasir: Force Int Added for ID

--- src/module/property/property.repository.ts
+++ src/module/property/property.repository.ts
@@ -1469,13 +1469,13 @@ export class PropertyRepository implements IPropertyRepository {
             // Update property with IDs if they exist
             const propertyUpdateData: any = {};
             if (rowData['Expedia ID']) {
-              propertyUpdateData.expedia_id = rowData['Expedia ID'];
+              propertyUpdateData.expedia_id = parseInt(rowData['Expedia ID']);
             }
             if (rowData['Booking ID']) {
-              propertyUpdateData.booking_id = rowData['Booking ID'];
+              propertyUpdateData.booking_id = parseInt(rowData['Booking ID']);
             }
             if (rowData['Agoda ID']) {
-              propertyUpdateData.agoda_id = rowData['Agoda ID'];
+              propertyUpdateData.agoda_id = parseInt(rowData['Agoda ID']);
             }
 
             // Update property if there are IDs to update
